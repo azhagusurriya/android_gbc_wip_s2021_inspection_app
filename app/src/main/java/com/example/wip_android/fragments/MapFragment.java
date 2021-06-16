643aff9f490d671d18b1efdfdd125381f474bf04
azhagusurriya <surriyaraj@gmail.com>
2021-06-16 03:59:16 -0400

    Map updated

--- app/src/main/java/com/example/wip_android/fragments/MapFragment.java
+++ app/src/main/java/com/example/wip_android/fragments/MapFragment.java
@@ -1,7 +1,12 @@
 package com.example.wip_android.fragments;
 
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +16,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.wip_android.R;
 import com.example.wip_android.viewmodels.MapViewModel;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    MapView mMapView;
+    View mView;
+    private final String TAG = this.getClass().getCanonicalName();
+    private Geocoder geocoder;
+    private final Float DEFAULT_ZOOM = 15.0F;
+    private LocationManager locationManager;
+    private LocationCallback locationCallback;
+    private LatLng currentLocation;
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
 
     private MapViewModel mViewModel;
 
@@ -23,10 +59,48 @@ public class MapFragment extends Fragment {
         return new MapFragment();
     }
 
+
+
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,
+                              Bundle savedInstanceState) {
+        mView =  inflater.inflate(R.layout.fragment_map, container, false);
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady( GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney").snippet("My Favt place"));
+
+        CameraPosition cSydney = CameraPosition.builder().target(new LatLng(-33.852, 151.211)).zoom(16).bearing(0).tilt(45).build();
+
+        // Move the camera to the marker
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cSydney));
+
+        // Zoom in, animating the camera.
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
     }
 
     @Override
@@ -34,6 +108,9 @@ public class MapFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MapViewModel.class);
         // TODO: Use the ViewModel
+
     }
+
+
 
 }
