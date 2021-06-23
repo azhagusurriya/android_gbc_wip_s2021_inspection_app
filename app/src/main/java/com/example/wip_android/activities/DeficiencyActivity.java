a240cc931c840ef6a9b691c0fab696024208b7d2
Matheus Hanssen <matheus.hanssen@hotmail.com>
2021-06-23 02:52:57 -0400

    issue screen added

--- app/src/main/java/com/example/wip_android/activities/DeficiencyActivity.java
+++ app/src/main/java/com/example/wip_android/activities/DeficiencyActivity.java
@@ -8,9 +8,16 @@ import com.example.wip_android.R;
 
 public class DeficiencyActivity extends AppCompatActivity {
 
+    // Default Function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deficiency);
     }
+
+    // Go to glossary screen
+    public void selectGlossary() {
+        System.out.println("test");
+    }
+
 }
