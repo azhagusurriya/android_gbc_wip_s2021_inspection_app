f77d6afd5aa4d2a0e1b0226d57d6e93392ba07fc
azhagusurriya <surriyaraj@gmail.com>
2021-07-15 15:33:56 -0400

    Home screen of ListItem updated

--- app/src/main/java/com/example/wip_android/fragments/DeficiencyImageViewFragment.java
+++ app/src/main/java/com/example/wip_android/fragments/DeficiencyImageViewFragment.java
@@ -11,13 +11,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.wip_android.R;
 import com.example.wip_android.viewmodels.DeficiencyImageViewViewModel;
 
 public class DeficiencyImageViewFragment extends Fragment {
 
     private DeficiencyImageViewViewModel mViewModel;
+    private ImageView deficiency_imageView;
 
     public static DeficiencyImageViewFragment newInstance() {
         return new DeficiencyImageViewFragment();
@@ -27,6 +30,13 @@ public class DeficiencyImageViewFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.deficiency_image_view_fragment, container, false);
+
+
+        this.deficiency_imageView = view.findViewById(R.id.deficiency_imageView);
+
+        Glide.with(this).load(this.getArguments().getString("image")).into(deficiency_imageView);
+
+
 
         return view;
     }
