57583973176bebbd9062b8013884ca04d819fc32
Matheus Hanssen <matheus.hanssen@hotmail.com>
2021-07-29 16:06:23 -0400

    list added

--- app/src/main/java/com/example/wip_android/fragments/DeficiencyListFragment.java
+++ app/src/main/java/com/example/wip_android/fragments/DeficiencyListFragment.java
@@ -3,6 +3,7 @@ package com.example.wip_android.fragments;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -10,21 +11,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.wip_android.R;
+import com.example.wip_android.models.DeficiencyCompletion;
 import com.example.wip_android.models.DeficiencyInfo;
 import com.example.wip_android.models.ProjectInfo;
+import com.example.wip_android.models.States;
 import com.example.wip_android.viewmodels.DeficiencyListViewModel;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class DeficiencyListFragment extends Fragment {
 
@@ -37,29 +47,158 @@ public class DeficiencyListFragment extends Fragment {
     private String COLLECTION_PROJECT = "Project";
     private String COLLECTION_DEFICIENCY = "Deficiencies";
     private String clientName;
+    ListView listView;
+    MyCustomAdapter dataAdapter = null;
 
+    // Fragment settings
     public static DeficiencyListFragment newInstance() {
         return new DeficiencyListFragment();
     }
 
+    // Default function
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.deficiency_list_fragment, container, false);
 
-        clientName = this.getArguments().getString("name");
-        System.out.println(clientName);
-        getDeficiencyFromFirebase();
+        // Generate list View from ArrayList
+        listView = (ListView) view.findViewById(R.id.listView);
+        displayListView();
 
-        ListView listView = (ListView) view.findViewById(R.id.listView);
-        adapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_multiple_choice,
-                arrayDeficiency);
-        listView.setAdapter(adapter);
+//        checkButtonClick();
+
+
+
+        // Get variables
+        clientName = this.getArguments().getString("name");
+
+        //        getDeficiencyFromFirebase();
+
+
+//        adapter = new ArrayAdapter<String>(
+//                getActivity(),
+//                android.R.layout.simple_list_item_multiple_choice,
+//                arrayDeficiency);
+//        listView.setAdapter(adapter);
 
         return view;
     }
+
+    private void displayListView() {
+
+        // Array list of countries
+        ArrayList<States> stateList = new ArrayList<States>();
+
+        States _states = new States("91", "India", false);
+        stateList.add(_states);
+        _states = new States("61", "Australia", true);
+        stateList.add(_states);
+        _states = new States("55", "Brazil", false);
+        stateList.add(_states);
+        _states = new States("86", "China", true);
+        stateList.add(_states);
+        _states = new States("49", "Germany", true);
+        stateList.add(_states);
+        _states = new States("36", "Hungary", false);
+        stateList.add(_states);
+        _states = new States("39", "Italy", false);
+        stateList.add(_states);
+        _states = new States("1", "US", false);
+        stateList.add(_states);
+        _states = new States("44", "UK", false);
+        stateList.add(_states);
+
+        // create an ArrayAdaptar from the String Array
+        dataAdapter = new MyCustomAdapter(getActivity(), R.layout.state_info, stateList);
+//        ListView listView = (ListView) findViewById(R.id.listView);
+        // Assign adapter to ListView
+        listView.setAdapter(dataAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                States state = (States) parent.getItemAtPosition(position);
+//                Toast.makeText(getApplicationContext(),
+//                        "Clicked on : " + state.getName(), Toast.LENGTH_LONG)
+//                        .show();
+            }
+        });
+    }
+
+    private class MyCustomAdapter extends ArrayAdapter<States> {
+
+        private ArrayList<States> stateList;
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+
+                               ArrayList<States> stateList) {
+            super(context, textViewResourceId, stateList);
+            this.stateList = new ArrayList<States>();
+            this.stateList.addAll(stateList);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+
+                LayoutInflater vi = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                convertView = vi.inflate(R.layout.state_info, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView
+                        .findViewById(R.id.checkBox1);
+
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v;
+                        States _state = (States) cb.getTag();
+
+                        System.out.println(cb.getText());
+
+//                        Toast.makeText(
+//                                getApplicationContext(),
+//                                "Checkbox: " + cb.getText() + " -> "
+//                                        + cb.isChecked(), Toast.LENGTH_LONG)
+//                                .show();
+
+                        _state.setSelected(cb.isChecked());
+                    }
+                });
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            States state = stateList.get(position);
+
+            holder.code.setText(" (" + state.getCode() + ")");
+            holder.name.setText(state.getName());
+            holder.name.setChecked(state.isSelected());
+
+            holder.name.setTag(state);
+
+            return convertView;
+        }
+
+    }
+
+
 
     // Upload information to Firebase Database
     public void getDeficiencyFromFirebase() {
